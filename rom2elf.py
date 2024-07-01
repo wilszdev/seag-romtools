0b9294db3090fb226271aebc3a566ae99c61f35c
wilszdev <wilszdev>
2024-07-01 22:11:42 +1000

    add extra segments to final binary via command line

--- rom2elf.py
+++ rom2elf.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+import argparse
 import importlib
 import struct
 import sys
@@ -29,39 +29,64 @@ unlzma = importlib.import_module("seag-lzma.unlzma")
 
 ERR_OK        = 0x00
 ERR_USAGE     = 0x01
-ERR_ROM_FILE  = 0x02
+ERR_IN_FILE   = 0x02
 ERR_ROM2ELF   = 0x04
 ERR_OUT_FILE  = 0x08
 
 
 def main() -> int:
-    if len(sys.argv) not in (2, 3):
-        sys.stderr.write(f'Usage: {sys.argv[0]} INPUTFILE [OUTPUTFILE]\n')
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description='Convert Seagate ROM binary to ELF')
+
+    parser.add_argument('-i', '--inputfile', required=False, help='Path to the input file. If unspecified, stdin is used.')
+    parser.add_argument('-o', '--outputfile', required=False, help='Path to the output file. If unspecified, stdout is used.')
+
+    parser.add_argument('-r', '--resolve', action='store_true', help='Resolve overlapping memory segments')
+
+    parser.add_argument('segments', nargs='*', help='Pairs of memory offsets (in hex) and paths to binary files', type=str)
+
+    args = parser.parse_args()
+
+    if len(args.segments) % 2 != 0:
+        sys.stderr.write('memory offsets and file paths must be provided in pairs\n')
         return ERR_USAGE
 
+    extraSegments = []
+    for i in range(0, len(args.segments), 2):
+        address = int(args.segments[i], 16)
+        path = args.segments[i + 1]
+        try:
+            with open(path, 'rb') as file:
+                data = file.read()
+            extraSegments.append((address, data))
+        except OSError:
+            sys.stderr.write(f'Error: Unable to open file {path}\n')
+            return ERR_IN_FILE
+
+    args.inputfile = 'samples/rom5_patched.bin'
+
     try:
-        inputFile = sys.stdin.buffer if sys.argv[1] == '-' else open(sys.argv[1], 'rb')
+        inputFile = open(args.inputfile, 'rb') if args.inputfile else sys.stdin.buffer
     except OSError:
-        sys.stderr.write(f'Error: Unable to open file {sys.argv[1]}\n')
-        return ERR_ROM_FILE
+        sys.stderr.write(f'Error: Unable to open file {args.inputfile}\n')
+        return ERR_IN_FILE
 
     with inputFile:
         inputData = inputFile.read()
 
-    if not (elf := rom2elf(inputData)):
+    if not (elf := rom2elf(inputData, extraSegments, args.resolve)):
         return ERR_ROM2ELF
 
     try:
-        outputFile = sys.stdout.buffer if len(sys.argv) == 2 else open(sys.argv[2], 'wb')
+        outputFile = open(args.outputfile, 'wb') if args.outputfile else sys.stdout.buffer
     except OSError:
-        sys.stderr.write(f'Error: Unable to open file {sys.argv[2]} for writing\n')
+        sys.stderr.write(f'Error: Unable to open file {args.outputfile} for writing\n')
         return ERR_OUT_FILE
 
     with outputFile:
         writeCount = outputFile.write(elf)
 
     if writeCount != len(elf):
-        sys.stderr.write(f'Error: Failed to write all data to file {sys.argv[2]}\n')
+        sys.stderr.write(f'Error: Failed to write all data to file {args.outputfile}\n')
         return ERR_OUT_FILE
 
     return ERR_OK
@@ -94,18 +119,18 @@ class Elf32:
         self.segments.append((data, address))
 
     @staticmethod
-    def elf32_header(osabi, type, machine, entry, phoff, shoff, flags, phnum, shnum, shstrndx):
+    def elf32_header(osabi, type, machine, entry, phoff, shoff, flags, phnum, shnum, shstrndx) -> bytes:
         return struct.pack('<8B8xHH5I6H',
                 0x7f, ord('E'), ord('L'), ord('F'), 1, 1, 1, osabi,
                 type, machine, 1, entry, phoff, shoff,
                 flags, 0x34, 0x20, phnum, 0x28, shnum, shstrndx)
 
     @staticmethod
-    def program_header(type, offset, vaddr, paddr, filesz, memsz, flags=PF_X|PF_W|PF_R, align=0):
+    def program_header(type, offset, vaddr, paddr, filesz, memsz, flags=PF_X|PF_W|PF_R, align=0) -> bytes:
         return struct.pack('<8I', type, offset, vaddr, paddr, filesz, memsz, flags, align)
 
     @staticmethod
-    def section_header(name, type, flags, addr, offset, size, link, info, addralign, entsize=0):
+    def section_header(name, type, flags, addr, offset, size, link, info, addralign, entsize=0) -> bytes:
         return struct.pack('<10I', name, type, flags, addr, offset, size, link, info, addralign, entsize)
 
     def resolve_segment_overlaps(self) -> None:
@@ -172,7 +197,7 @@ class Elf32:
         return blob
 
 
-def rom2elf(data: bytes) -> bytes:
+def rom2elf(data: bytes, extraSegments: list[tuple[int, bytes]], resolve=False) -> bytes:
     root = build_root_container(data)
 
     # find all the Files
@@ -191,17 +216,21 @@ def rom2elf(data: bytes) -> bytes:
 
     elf = Elf32()
 
-    for file in files:
-        data = file.blob
-        if file.packed and data[:4] == b'CPRS':
+    segments = [(file.loadAddress, file.blob, file.packed) for file in files]
+    segments += [(a, b, False) for a, b in extraSegments]
+
+    for address, data, packed in segments:
+        if packed and data[:4] == b'CPRS':
             data = uncprs.decompress(data)
-        elif file.packed and data[:4] == b'LZMA':
+        elif packed and data[:4] == b'LZMA':
             data = unlzma.decompress(data)
-        elf.add_segment(data, file.loadAddress)
+        elf.add_segment(data, address)
+
+    if resolve:
+        elf.resolve_segment_overlaps()
 
     return elf.to_blob()
 
 
 if __name__ == '__main__':
     exit(main())
-
